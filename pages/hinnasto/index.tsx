a2f8ef8dbcbba2b1b77e0a4949ce3dcf85bae87f
Lotta Lappalainen <lotta.lyy00@gmail.com>
2026-01-13 18:52:39 +0200

    Update new prices

--- pages/hinnasto/index.tsx
+++ pages/hinnasto/index.tsx
@@ -13,30 +13,35 @@ const Hinnasto: NextPage = () => {
         <h2>Hinnasto</h2>
         <p>
           Yritykset, yksityishenkilöt: <br />
-          To, Su: 620€
+          Su-To: 700€
           <br />
-          Pe, La: 720€
+          Pe-La: 790€
         </p>
         <p>
-					Ainejärjestöt/killat, korkeakouluopiskelijat:
+		  Ainejärjestöt/killat, korkeakouluopiskelijat ja Luuppi-alumnit:
           <br />
-          To, Su: 570€
+          Su-To: 560€
           <br />
-          Pe, La: 600€
+          Pe-La: 650€
         </p>
         <p>
-          Luuppilaiset:
+          Luuppi-opiskelijat*:
           <br />
-          To - Su: 350€
+          Su-To: 420€
+		  <br />
+          Pe-La: 510€
         </p>
         <p>
-          Hintoihin lisätään sesonkilisä 100€ marras- ja joulukuussa. <br />
-          Maanantaisin, tiistaisin ja keskiviikkoisin tila ei ole
-          oletusarvoisesti varattavissa. <br />
-          Huom! Wapun aikana (13.4. - 1.5.) 
-          tila on varattavissa viikon jokaisena päivänä. <br />
-          Voit kuitenkin kysellä mahdollisuuksia{" "}
-          <a href="mailto: toimitila@luuppi.fi">toimitila@luuppi.fi!</a>
+		  Mikäli olet saanut varausvahvistuksen ennen 13.1.2026, käytetään vuokrauksessa vanhaa hinnastoa.
+		  <br />
+		  HUOM! Maanantaisin, tiistaisin ja keskiviikkoisin tila ei ole oletusarvoisesti varattavissa. 
+		  Voit kuitenkin kysellä mahdollisuuksia toimitila@luuppi.fi!
+		  Wapun aikana (13.4. - 1.5.) tila on varattavissa viikon jokaisena päivänä. 
+		  <br />
+		  Hintoihin lisätään sesonkilisä 100€ marras- ja joulukuussa. 
+		  Vuokraus sisältää 12h vuokra-aikaa, jonka jälkeen lisätunteja on mahdollista ostaa hintaan 50€/alkava 4h.
+		  <br />
+		  *Luupin jäsenet, joilla on aktiivinen opiskeluoikeus.
         </p>
       </div>
     </SideImageLayout>
